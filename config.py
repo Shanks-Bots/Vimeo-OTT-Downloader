e4d2b573ea90f082d31605955b379c11ded60370
Shanks-Bots <112746600+Shanks-Bots@users.noreply.github.com>
2022-09-10 09:45:59 +0545

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+platform_cookies = {
+    'tracker': 'xxx',
+    'locale_det': 'en',
+    '__stripe_mid': 'xxx',
+    '_session': 'xxx',
+    '__cf_bm': 'xxx',
+    '__stripe_sid': 'xxx',
+    'referrer_url': 'xxx',
+}
+
+platform_headers = {
+    'authority': 'xxx',
+    'referer': 'xxx',
+}
+
+out = "X:\\Downloads\\"
+url = ["https://xyz.xyz/xyz", "https://xyz.xyz/abc"]
